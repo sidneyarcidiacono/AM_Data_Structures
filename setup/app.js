0c172299a220545052fd8611fe8f07495dd512bf
Sidney Arcidiacono <sidneyarci@gmail.com>
2020-12-08 11:24:50 -0800

    Finished graph implementation & optimized performance by refactoring to use sets rather than arrays

--- setup/app.js
+++ setup/app.js
@@ -5,15 +5,47 @@ class Graph {
   }
 
   addNode (id, value) {
+    if (this.nodes[id]) {
+      throw new Error('Node exists already!')
+    }
     this.nodes[id] = value
   }
 
   addEdge (startNode, endNode) {
-    if (this.edges[startNode] && this.edges[startNode].indexOf(endNode) === -1) {
-      this.edges[startNode].push(endNode)
-    } else {
-      this.edges[startNode] = [endNode]
+    if (!this.nodes[startNode] || !this.nodes[endNode]) {
+      throw new Error('Start or end node does not exist!')
     }
+    if (this.edges[startNode] && !this.edges[startNode].has(endNode)) {
+      this.edges[startNode].add(endNode)
+    } else {
+      this.edges[startNode] = new Set([endNode])
+    }
+  }
+
+  removeNode (nodeId) {
+    this.nodes[nodeId] = undefined
+    Reflect.deleteProperty(this.edges, nodeId)
+    for (const edgeId in this.edges) {
+      this.edges[edgeId].delete(nodeId)
+    }
+  }
+
+  removeEdge (startNode, endNode) {
+    if (!this.edges[startNode] || !this.edges[startNode].has(endNode)) {
+      throw new Error('Edge does not exist!')
+    }
+    this.edges[startNode].delete(endNode)
+  }
+
+  hasEdge(startNode, endNode) {
+    if (!this.edges[startNode]) {
+      return false
+    }
+    return this.edges[startNode].has(endNode)
+  }
+
+  getAllEdges(node) {
+    return this.edges[node]
   }
 }
 
@@ -26,5 +58,17 @@ graph.addNode(3, 'Jules')
 graph.addEdge(1, 2)
 graph.addEdge(1, 3)
 graph.addEdge(3, 2)
+
+console.log(graph.hasEdge(3, 2))
+console.log(graph.hasEdge(2, 1))
+
+console.log(graph.getAllEdges(1))
+console.log(graph.getAllEdges(2))
+console.log(graph.getAllEdges(3))
+
+graph.removeNode(2)
+
+// graph.removeEdge(2, 1)
+// graph.removeEdge(1, 2)
 
 console.log(graph)
