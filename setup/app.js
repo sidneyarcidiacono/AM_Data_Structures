f99096df3e3581e1a979c3c634e7ae72ead26ca3
Sidney Arcidiacono <sidneyarci@gmail.com>
2020-12-08 08:52:19 -0800

    Updated linked list to include prepend & delete

--- setup/app.js
+++ setup/app.js
@@ -16,6 +16,40 @@ class LinkedList {
     }
   }
 
+  prepend(value) {
+    const newNode = {value: value, next: this.head}
+
+    this.head = newNode
+
+    if (!this.tail) {
+      this.tail = newNode
+    }
+  }
+
+  delete(value) {
+    if (!this.head) {
+      return null
+    }
+
+    while (this.head && this.head.value === value) {
+      this.head = this.head.next
+    }
+
+    let currentNode = this.head
+
+    while (currentNode.next) {
+      if (currentNode.next.value === value) {
+        currentNode.next = currentNode.next.next
+      } else {
+        currentNode = currentNode.next
+      }
+    }
+
+    if (this.tail.value === value) {
+      this.tail = currentNode
+    }
+  }
+
   toArray() {
     const elements = []
 
@@ -33,5 +67,15 @@ const linkedList1 = new LinkedList()
 linkedList1.append(1)
 linkedList1.append(2)
 linkedList1.append(3)
+linkedList1.append('Max')
+linkedList1.append('Max')
+linkedList1.append(true)
+linkedList1.prepend(0)
+
+console.log(linkedList1.toArray())
+
+linkedList1.delete(0)
+linkedList1.delete('Max')
+linkedList1.delete(true)
 
 console.log(linkedList1.toArray())
